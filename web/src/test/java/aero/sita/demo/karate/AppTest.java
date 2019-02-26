51cf0387641738a6eb1c62fe7fc97072004c5e8e
Romero Ricardo @ CINQ <r.sousa@cinq.com.br>
2019-02-26 18:41:59 -0300

    Initial Project Structure

---
+++ web/src/test/java/aero/sita/demo/karate/AppTest.java
@@ -0,0 +1,20 @@
+package aero.sita.demo.karate;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
